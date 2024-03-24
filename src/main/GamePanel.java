6039c4450af0c43144d305cb2bf60d2e07c8578c
melgohary2715 <melgohary2715@gmail.com>
2024-03-24 15:18:28 -0400

    panel working

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -12,7 +12,7 @@ public class GamePanel extends JPanel{
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
 
-    g.drawRect(100, 100, 200, 50);
+    g.fillRect(100, 100, 200, 50);
   }
 
 }
